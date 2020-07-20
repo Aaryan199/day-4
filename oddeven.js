637c349bbbefb328e4e981e944f8be6879364338
Aaryan199 <68271227+Aaryan199@users.noreply.github.com>
2020-07-20 10:34:28 +0530

    Add files via upload

---
+++ oddeven.js
@@ -0,0 +1,7 @@
+let num=prompt("What is your num:");
+ if (num%=0){
+  console.log(`The number is even`);
+ }
+ else {
+  console.log(`The number is odd`);
+  }
---
+++ shopping.js
@@ -0,0 +1,12 @@
+let shoppinglist=['dal','rice','flour']
+//while(true){
+let y=prompt('What do you want to add?')
+//  if(y==done){
+//    break}
+//  else{continue}
+//}
+shoppingbasket=[]
+shoppingbasket==shoppinglist
+debugger
+shoppingbasket.upshift(y);
+console.log(shoppingbasket)
